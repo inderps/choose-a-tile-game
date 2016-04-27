1da66ac388e464cd95db5824439779fa49b05199
Inderpal Singh <singhinther@gmail.com>
2016-04-27 15:39:47 +0200

    corrected path

--- src/reducers/gameSession.js
+++ src/reducers/gameSession.js
@@ -6,54 +6,54 @@ const initialState = {
   rounds: [{
     rows: [{
       tiles: [{
-        url: '/tiles/111.jpg',
+        url: 'tiles/111.jpg',
       }, {
-        url: '/tiles/112.jpg',
+        url: 'tiles/112.jpg',
         isCorrect: true,
       }],
     }, {
       tiles: [{
-        url: '/tiles/121.jpg',
+        url: 'tiles/121.jpg',
       }, {
-        url: '/tiles/122.jpg',
+        url: 'tiles/122.jpg',
       }],
     }],
   }, {
     rows: [{
       tiles: [{
-        url: '/tiles/211.jpg',
+        url: 'tiles/211.jpg',
       }, {
-        url: '/tiles/212.jpg',
+        url: 'tiles/212.jpg',
       }],
     }, {
       tiles: [{
-        url: '/tiles/221.jpg',
+        url: 'tiles/221.jpg',
         isCorrect: true,
       }],
     }, {
       tiles: [{
-        url: '/tiles/231.jpg',
+        url: 'tiles/231.jpg',
       }, {
-        url: '/tiles/232.jpg',
+        url: 'tiles/232.jpg',
       }, {
-        url: '/tiles/233.jpg',
+        url: 'tiles/233.jpg',
       }],
     }],
   }, {
     rows: [{
       tiles: [{
-        url: '/tiles/311.jpg',
+        url: 'tiles/311.jpg',
       }],
     }, {
       tiles: [{
-        url: '/tiles/321.jpg',
+        url: 'tiles/321.jpg',
       }, {
-        url: '/tiles/322.jpg',
+        url: 'tiles/322.jpg',
         isCorrect: true,
       }, {
-        url: '/tiles/323.jpg',
+        url: 'tiles/323.jpg',
       }, {
-        url: '/tiles/324.jpg',
+        url: 'tiles/324.jpg',
       }],
     }],
   }],
